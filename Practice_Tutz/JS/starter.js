b4d7f5372ce213845bd91f2f5a80712537857226
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-07-13 21:17:45 +0300

    JS Foundation - Building blocks Part I

---
+++ Practice_Tutz/JS/starter.js
@@ -0,0 +1,13 @@
+// Primitive Types
+// Numbers - Integers and Floats
+// Strings
+// Booleans
+// Null
+console.log("Hello World");
+
+function calc() {
+    alert("Add 2 Numbers");
+    let num1 = Number(prompt("Enter number: "));
+    let num2 = Number(prompt("Enter number: "));
+    alert(String(num1) + " + " + String(num2) + " = " + (num1 + num2));
+}
