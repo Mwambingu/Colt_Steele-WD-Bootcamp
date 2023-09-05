b0fa9a90b0255dd518723d5ff017b8cdd6e07a7e
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-09-05 20:25:41 +0300

    The Terminal and NodeJS

---
+++ Practice_Tutz/Node/starter.js
@@ -0,0 +1,39 @@
+// argv -> returns an array of the command line args
+console.log("Call me if you get lost!!");
+
+if (Number(process.argv[2]) && Number(process.argv[3])) {
+    const add = (a, b) => Number(a) + Number(b);
+
+    console.log(add(process.argv[2], process.argv[3]));
+}
+
+const args = process.argv.slice(2);
+
+// File System module
+// fs
+const fs = require("fs");
+const fsLocation = `/home/somboch/my_repo/Colt_Steele-WD-Bootcamp/Practice_Tutz/Node/`;
+
+function makeWebDir(location) {
+    fs.mkdirSync(`${location}web_app`, { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+    fs.opendir("web_app", (err, dir) => {
+        if (err) console.log(`Error: ${err}`);
+        else {
+            console.log("Path:", dir.path);
+        }
+    });
+
+    fs.writeFileSync("./web_app/app.js", "", (err) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync("./web_app/app.css", "", (err) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync("./web_app/index.html", "", (err) => {
+        if (err) throw err;
+    });
+}
+
+makeWebDir(fsLocation);
