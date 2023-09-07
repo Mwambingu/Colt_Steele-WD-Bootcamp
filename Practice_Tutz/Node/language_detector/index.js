4a827823c20f41f95cfefcd0ecf826f5073bdb24
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-09-07 17:39:32 +0300

    Completed language detector and working with dependencies in NPM

---
+++ Practice_Tutz/Node/language_detector/index.js
@@ -0,0 +1,31 @@
+// console.log(
+//   franc(
+//       "tarakilishi hutumika ofisini. Walimu na wanafunzi hupenda kutumia tarakilishi sana. Tunaomba serikali waendele na kazi mzuri. Yakueneza demokrasia ya teknolojia nchi nzima"
+//   )
+// );
+// console.log(franc("Alle menslike wesens word vry"));
+
+const franc = require("franc");
+const langs = require("langs");
+const colors = require("colors");
+const args = process.argv.slice(2);
+const sentence = args[0];
+
+const translate = (sentence) => {
+    try {
+        if (sentence) {
+            let langISO = franc(sentence);
+            if (langISO === "swh") {
+                langISO = "swa";
+            }
+            let langObj = langs.where("3", langISO);
+            let langName = langObj.name;
+            console.log(langName.bold.green);
+        }
+        return;
+    } catch (err) {
+        console.log("Error: Couldn't find translation!!".bold.red);
+    }
+};
+
+translate(sentence);
