d99c355d4961e2ea2f561da98755d4fd78a57298
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-09-07 20:18:02 +0300

    Getting started with Express, Create apps, Handling requests, responses, routing and Path parameters. Auto restart Express with nodemon

---
+++ Practice_Tutz/Express/app_one/index.js
@@ -0,0 +1,68 @@
+// Creating an Express App
+const express = require("express");
+const app = express();
+
+// Accepting requests and giving responses using use
+// Using the request and response object provided by Express
+// app.use((req, res) => {
+//     console.log("We got a new request");
+//     res.send("<h1>Hello World</h1>");
+// });
+
+// Routing with Express
+app.get("/cats", (req, res) => {
+    console.log("cat request...");
+    res.send("<h1>We are the Cat Imperium. Mortal bow!!</h1>");
+});
+
+app.post("/", (req, res) => {
+    console.log("Sending home msg...");
+    res.send("<h1>There no one here hmmmm...</h1>");
+});
+
+app.get("/dogs", (req, res) => {
+    console.log("dog request...");
+    res.send("<h1>We are the Dog Guardians. At your service Master!!</h1>");
+});
+
+app.get("/", (req, res) => {
+    console.log("home request...");
+    res.send("<h1>I'm Home at last. Anybody here?</h1>");
+});
+
+// Path Parameters
+// (:) - Using variable paths
+app.get("/people/:person", (req, res) => {
+    const { person } = req.params;
+    console.log(`Looking for person...`);
+    res.send(`<h1>This is the person: ${person}</h1>`);
+});
+
+app.get("/people/:person/:house", (req, res) => {
+    const { person, house } = req.params;
+    console.log(`Person showing house...`);
+    res.send(`<h1>I am ${person} and this is my ${house}</h1>`);
+});
+
+// Working with query strings
+// req object has the query property
+// property contains the key value pairs of the query string
+app.get("/search", (req, res) => {
+    console.log("Running a search...", req.query);
+    const { q } = req.query;
+    if (!q) {
+        res.send(`<h1>Nothing found!! No search param!!</h1>`);
+    } else {
+        res.send(`<h1>Search results on: ${q}</h1>`);
+    }
+});
+
+// Matching every single request
+app.get("*", (req, res) => {
+    console.log("weird request..");
+    res.send("<h1>I think you might be lost.</h1>");
+});
+// Listening on ports
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
