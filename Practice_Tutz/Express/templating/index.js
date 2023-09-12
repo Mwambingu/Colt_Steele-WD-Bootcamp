0d2552986ca3670657105564d32ee7c0ec79272c
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-09-12 19:43:02 +0300

    Refactored code based on Colt's solution

--- Practice_Tutz/Express/templating/index.js
+++ Practice_Tutz/Express/templating/index.js
@@ -6,11 +6,10 @@ const app = express();
 const port = 3000;
 
 // console.log(jsonFile);
+// Pulling data from JSON file for the reddit clone app
 let jsonData = JSON.parse(fs.readFileSync("./data.json"));
 
-const { soccer, chickens, mightyharvest } = jsonData;
-
-console.log(soccer, chickens, mightyharvest);
+// const { soccer, chickens, mightyharvest } = jsonData;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -26,29 +25,15 @@ app.get("/cats", (req, res) => {
     res.render("cats", { cats });
 });
 
+// Routing to the reddit clone app
 app.get("/reddit/:subreddit", (req, res) => {
-    let { subreddit } = req.params;
+    const { subreddit } = req.params;
     let subPage = null;
-
-    if (subreddit === "soccer") {
-        subPage = soccer;
-        res.render("reddit", { subPage, subreddit });
-        return;
+    if (jsonData[subreddit]) {
+        subPage = jsonData[subreddit];
+        res.render("reddit", { subPage });
     }
-    if (subreddit === "chickens") {
-        subPage = chickens;
-        res.render("reddit", { subPage, subreddit });
-        return;
-    }
-    if (subreddit === "mightyharvest") {
-        subPage = mightyharvest;
-        res.render("reddit", { subPage, subreddit });
-        return;
-    }
-
-    subreddit = null;
-
-    res.render("reddit", { subreddit });
+    res.render("reddit", { subPage });
 });
 
 app.get("/r/:subreddit", (req, res) => {
