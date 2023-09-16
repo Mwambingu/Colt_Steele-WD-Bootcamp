24754a66b47a32202631d211315cf5d4151cb3e6
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-09-16 14:10:02 +0300

    Express Method Overrides, Handling both PUT or PATCH, Delete requests and Completely built a fully CRUD REST type Comments API.

--- Practice_Tutz/Express/rest/comment_api/index.js
+++ Practice_Tutz/Express/rest/comment_api/index.js
@@ -7,6 +7,7 @@
 
 const express = require("express");
 const { v4: uuid4 } = require("uuid");
+const methodOverride = require("method-override");
 const path = require("path");
 const comments = [
     {
@@ -38,11 +39,41 @@ const comments = [
     },
 ];
 
+// Replace element in an array verbose version
+function replaceArrayItem(arr, targetValue, newValue) {
+    const new2DArr = arr.map((arritem, arrindex) => [arritem, arrindex]);
+    console.log(new2DArr);
+    const matchArr = new2DArr.find((arrItem) => arrItem[0] === targetValue);
+    console.log(matchArr);
+    const matchArrIndex = matchArr[1];
+    console.log(matchArrIndex);
+
+    arr[matchArrIndex] = newValue;
+
+    return arr;
+}
+
+// replace element in an array clean version
+function repArrItm(arr, tVal, nVal) {
+    arr[arr.map((x, i) => [x, i]).find((x) => x[0] === tVal)[1]] = nVal;
+}
+
+// replace old comment with new comment
+function replaceComment(comments, commentId, newComment) {
+    console.log("Hello");
+    const commentIndex = comments
+        .map((comment, index) => [comment, index])
+        .find((comment) => comment[0].id === commentId)[1];
+
+    comments[commentIndex].comment = newComment;
+}
+count = 0;
 app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(methodOverride("_method"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -61,17 +92,19 @@ app.get("/comments/new", (req, res) => {
 
 app.get("/comments/:id", (req, res) => {
     const id = req.params.id;
-    const userData = comments.find((comment) => {
+    const comment = comments.find((comment) => {
         return comment.id === id;
     });
-
-    res.render("comments/show", { userData });
+    const commentIndex = comments.findIndex((comment) => comment.id === id);
+    res.render("comments/show", { comment });
 });
 
 app.get("/comments/:id/edit", (req, res) => {
     const id = req.params.id;
-    console.log(id);
-    res.render("comments/edit", { id });
+    const comment = comments.find((comment) => {
+        return comment.id === id;
+    });
+    res.render("comments/edit", { comment });
 });
 
 app.post("/comments", (req, res) => {
@@ -82,8 +115,23 @@ app.post("/comments", (req, res) => {
     res.redirect("/comments");
 });
 
-app.put("/comments", (req, res) => {
-    console.log(req.body);
+app.patch("/comments/:id", (req, res) => {
+    const id = req.params.id;
+    const newComment = req.body.comment;
+    const foundComment = comments.find((comment) => {
+        return comment.id === id;
+    });
+    foundComment.comment = newComment;
+    res.redirect("/comments");
+    // replaceComment(comments, comments[0].id, "16 Times the detail");
+});
+
+app.delete("/comments/:id", (req, res) => {
+    const id = req.params.id;
+    const commentIndex = comments.findIndex((comment) => comment.id === id);
+    const deletedComment = comments.splice(commentIndex, 1);
+    console.log(deletedComment);
+    res.redirect("/comments");
 });
 
 app.listen("3000", () => {
