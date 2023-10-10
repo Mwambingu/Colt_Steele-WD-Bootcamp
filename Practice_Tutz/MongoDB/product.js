d1205bde02d604c88e42e8911eb9fb6495587bbd
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-10-10 20:54:01 +0300

    Mongoose - Validating updates, handling validation errors and working with instance methods in MongoDB

--- Practice_Tutz/MongoDB/product.js
+++ Practice_Tutz/MongoDB/product.js
@@ -4,25 +4,66 @@ main().catch((err) => console.log(err));
 
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/shopApp");
+
+    const productsSchema = new mongoose.Schema({
+        name: { type: String, required: true },
+        price: {
+            online: {
+                type: Number,
+                required: true,
+                default: 0,
+                // Custom validation error messages
+                min: [
+                    0,
+                    "Heyoo since when did you see negative prices!! Ya bum!! Fogoff!!",
+                ],
+            },
+            inStore: { type: Number, required: true, default: 0, min: 0 },
+        },
+        quantity: { type: Number, required: true },
+        isOnSale: { type: Boolean, required: true, default: false },
+        categories: [String],
+        size: {
+            type: String,
+            enum: ["250ml", "300ml"],
+            default: "250ml",
+        },
+    });
+
+    // Instance methods
+    productsSchema.methods.onSale = function () {
+        console.log(
+            `${this.name} is on sale. Only at $${this.price.online * 0.8}`
+        );
+    };
+
+    productsSchema.methods.addCategory = function (newCat) {
+        this.categories.push(newCat);
+        this.save();
+    };
+
+    const Product = mongoose.model("Product", productsSchema);
+
+    const products = await Product.find({
+        name: "Nice & Lovely - Suppleness",
+    });
+
+    if (!products.length) {
+        const lotion = new Product({
+            name: "Nice & Lovely - Suppleness",
+            quantity: 40,
+            isOnSale: false,
+        });
+
+        lotion.save();
+    }
+    // Run validators to remind MongoDB to check validations when updating
+    let update = await Product.findOneAndUpdate(
+        { name: "Nice & Lovely - Suppleness" },
+        { $set: { "price.online": 300, size: "300ml" } },
+        { new: true, runValidators: true }
+    );
+    console.log(update);
+    update.onSale();
+    update.addCategory("Skin-care");
 }
-
-const productsSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: {
-        online: { type: Number, required: true, default: 0, min: 0 },
-        inStore: { type: Number, required: true, default: 0, min: 0 },
-    },
-    quantity: { type: Number, required: true },
-    isOnSale: { type: Boolean, required: true, default: false },
-    categories: [String],
-});
-
-const Product = mongoose.model("Product", productsSchema);
-
-const lotion = new Product({
-    name: "Nice & Lovely - Suppleness",
-    quantity: 40,
-    isOnSale: false,
-});
-
-lotion.save();
