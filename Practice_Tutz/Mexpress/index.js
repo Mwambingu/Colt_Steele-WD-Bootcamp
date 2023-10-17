7a4f04c191a7b13b846e359f43cabd0cea68dfa8
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-10-17 20:18:30 +0300

    Express and Mongoose creating a groceries api - setting up the index page

--- Practice_Tutz/Mexpress/index.js
+++ Practice_Tutz/Mexpress/index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const path = require("path");
+const { Product } = require("./models/product.js");
 const port = 3000;
 
 main().catch((err) => console.log(err));
 
 async function main() {
-    mongoose.connect("mongodb://localhost:27017/shopApp");
+    mongoose.connect("mongodb://localhost:27017/grocery");
     console.log("connected to db....");
 }
 
@@ -15,7 +16,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.render("index");
+});
+
+app.get("/products", async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
 });
 
 app.listen(port, () => {
