385b3036336110830c8802c8f53df1bf2a49ba91
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-10-23 20:30:30 +0300

    Finished styling the products page, created the show route to the show page, and created a table to show the data pulled from the db

--- Practice_Tutz/Mexpress/index.js
+++ Practice_Tutz/Mexpress/index.js
@@ -29,6 +29,13 @@ app.get("/products", async (req, res) => {
     res.render("./products/index", { products });
 });
 
+app.get("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findOne({ _id: id });
+    console.log(product);
+    res.render("./products/show", { product });
+});
+
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
