80bfada4e1a6493ddd5db39c85b29aafcfa9ec22
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-11-21 18:59:46 +0300

    Working with Morgan middleware

---
+++ Practice_Tutz/middleware/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+
+morgan("tiny");
+
+const app = express();
+const port = 3000;
+
+// app.use(() => {
+//     console.log("Use custom function called!!!!");
+// });
+
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log("Listening on port " + port);
+});
