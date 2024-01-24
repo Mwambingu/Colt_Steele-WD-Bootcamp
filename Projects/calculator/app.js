691941a8c430b89a5186e0c14e26e6cecd874b56
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2024-01-24 20:38:03 +0300

    Additional Styling and Feedback animations

--- Projects/calculator/app.js
+++ Projects/calculator/app.js
@@ -1,8 +1,25 @@
-const squareBtns = document.querySelectorAll(".square-btn");
+const numBtns = document.querySelectorAll(".square-btn");
 
-for (squareBtn of squareBtns) {
-    console.log(squareBtn.innerText);
-    squareBtn.addEventListener("click", () => {
-        alert("Hello");
+// for (numBtn of numBtns) {
+//     console.log(numBtn.innerText);
+//     numBtn.addEventListener("click", () => {
+//         numBtn.classList.add("animate-btn");
+//         console.log(numBtn.innerText);
+//     });
+// }
+
+numBtns.forEach((numBtn) => {
+    numBtn.addEventListener("click", () => {
+        numBtn.classList.add("animate-btn");
+        setTimeout(() => {
+            numBtn.classList.remove("animate-btn");
+        }, 100);
+        console.log(numBtn.innerText);
     });
-}
+});
+
+numBtns.forEach((numBtn) => {
+    numBtn.addEventListener("click", () => {
+        document.querySelector(".calc-screen").innerText += numBtn.innerText;
+    });
+});
