22aa372fa0a2d44823497abe7a3931c53e8606c1
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2024-01-29 21:03:35 +0300

    Added 2 digit calculation functionality

--- Projects/calculator/app.js
+++ Projects/calculator/app.js
@@ -1,4 +1,10 @@
 const allBtns = document.querySelectorAll(".square-btn");
+const numBtns = document.querySelectorAll(".nums");
+const screen = document.querySelector(".calc-screen");
+const signs = document.querySelectorAll(".signs");
+const resetBtn = document.querySelector(".reset-btn");
+const equalSign = document.querySelector(".equal-sign");
+let inputData = [];
 
 // for (numBtn of numBtns) {
 //     console.log(numBtn.innerText);
@@ -7,6 +13,22 @@ const allBtns = document.querySelectorAll(".square-btn");
 //         console.log(numBtn.innerText);
 //     });
 // }
+
+const calculate = (firstInput, secondInput) => {
+    let num1 = Number(firstInput[0]);
+    let sign = firstInput[1];
+    let num2 = Number(secondInput);
+
+    if (sign === "+") {
+        return num1 + num2;
+    } else if (sign === "-") {
+        return num1 - num2;
+    } else if (sign === "*") {
+        return num1 * num2;
+    } else {
+        return num1 / num2;
+    }
+};
 
 allBtns.forEach((allBtn) => {
     allBtn.addEventListener("click", () => {
@@ -20,6 +42,36 @@ allBtns.forEach((allBtn) => {
 
 numBtns.forEach((numBtn) => {
     numBtn.addEventListener("click", () => {
-        document.querySelector(".calc-screen").innerText += numBtn.innerText;
+        if (screen.innerText.length > 22) {
+            document.querySelector(".calc-screen").innerText =
+                "Error!! Exceeded Char Limit!!";
+        } else {
+            document.querySelector(".calc-screen").innerText +=
+                numBtn.innerText;
+        }
     });
+});
+
+signs.forEach((signs) => {
+    signs.addEventListener("click", () => {
+        let input = screen.innerText;
+        let sign = signs.innerText;
+        inputData.push(input, sign);
+        console.log(inputData);
+        screen.innerText = "";
+    });
+});
+
+equalSign.addEventListener("click", () => {
+    let num2 = screen.innerText;
+    result = calculate(inputData, num2);
+    screen.innerText = result;
+    inputData = [];
+});
+
+// Reset button functionality
+resetBtn.addEventListener("click", () => {
+    inputData = [];
+    console.log(inputData);
+    screen.innerText = "";
 });
